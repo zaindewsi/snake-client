26ae18c32f12d672ea079553d9e0d288e9eb74c9
Zain Dewsi <zain.dewsi@gmail.com>
2021-05-05 18:51:07 +0000

    updated initails argument

--- constants.js
+++ constants.js
@@ -22,7 +22,7 @@ const MESSAGES = {
 //NAME
 const args = process.argv.slice(2);
 let initials = "";
-if (args.length > 1 || args[0].length > 3) {
+if (args.length !== 1 || args[0].length > 3) {
   console.log(
     "Please enter initials as a single argument less than or equal to 3 characters"
   );
